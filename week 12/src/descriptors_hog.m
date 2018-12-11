c8462db24e2b6730cf8057e670d328689fe24513
Nicolas Marchal <nicolas.marchal@epfl.ch>
2018-12-11 17:15:36 +0100

    week 12 : local feature

--- week 12/src/descriptors_hog.m
+++ week 12/src/descriptors_hog.m
@@ -3,15 +3,33 @@ function [descriptors,patches] = descriptors_hog(img,vPoints,cellWidth,cellHeigh
     nBins = 8;
     w = cellWidth; % set cell dimensions
     h = cellHeight;   
-
-    descriptors = zeros(0,nBins*4*4); % one histogram for each of the 16 cells
-    patches = zeros(0,4*w*4*h); % image patches stored in rows    
+    N = max(size(vPoints)) ;
     
-    [grad_x,grad_y]=gradient(img);    
+    descriptors = zeros(N,nBins*4*4); % one histogram for each of the 16 cells
+    patches = zeros(N,4*w*4*h); % image patches stored in rows    
+    
+    [grad_x,grad_y]=gradient(img); 
+    grad_orientation = atan2(grad_y,grad_y) ;
     
     for i = [1:size(vPoints,1)] % for all local feature points
-        ...
-        ...
+        m=1 ;
+        n=1 ;
+        for j = -2:1
+            for k = -2:1
+                patch = img((vPoints(i,2)+k*h):(vPoints(i,2)+(k+1)*h-1), ...
+                            (vPoints(i,1)+j*w):(vPoints(i,1)+(j+1)*w-1)); 
+                patch_orientation = grad_orientation( ...
+                                (vPoints(i,2)+k*h):(vPoints(i,2)+(k+1)*h-1), ...
+                                (vPoints(i,1)+j*w):(vPoints(i,1)+(j+1)*w-1)) ;
+                descriptors(m:m+7) = histcounts(patch_orientation, ...
+                                                nBins,'BinLimits',[-pi,pi]);
+                patches(i,n:n + w*h-1) = reshape(patch,1,w*h) ;
+                m = m+8 ;
+                n = n + w*h ;
+            end
+        end
+        
+        
     end % for all local feature points
     
 end
--- week 12/src/grid_points.m
+++ week 12/src/grid_points.m
@@ -1,4 +1,12 @@
 function vPoints = grid_points(img,nPointsX,nPointsY,border)
+    [Ysize Xsize] = size(img) ;
     
+    X = int32(linspace(1+border, Xsize-border, nPointsX)) ;
+    Y = int32(linspace(1+border, Ysize-border, nPointsY)) ;
+    
+    [X,Y] = meshgrid(X,Y) ;
+    X = reshape(X,nPointsX*nPointsY,1) ;
+    Y = reshape(Y,nPointsY*nPointsX,1) ;
+    vPoints = [X, Y] ;
     
 end
